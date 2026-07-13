febbd2c34a45f9f0bb543ad9d1a83aa967b2dfc0
Yaswanth Gunakala <Yaswanthgunakala29@gmail.com>
2026-07-13 23:46:39 +0530

    Add: maximum-sum-circular-subarray - submission-0

---
+++ Data Structures & Algorithms/maximum-sum-circular-subarray/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxSubarraySumCircular(self, nums: List[int]) -> int:
+        globMax, globMin = nums[0], nums[0]
+        curMax, curMin = 0, 0
+        total = 0
+
+        for num in nums:
+            curMax = max(curMax + num, num)
+            curMin = min(curMin + num, num)
+            total += num
+            globMax = max(globMax, curMax)
+            globMin = min(globMin, curMin)
+
+        return max(globMax, total - globMin) if globMax > 0 else globMax
